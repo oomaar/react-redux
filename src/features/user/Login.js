bf60209538495ece34ccda035c5ccac630f92726
Omar <omaar_5@hotmail.com>
2021-01-31 16:22:35 +0200

    done redux basics

--- src/features/user/Login.js
+++ src/features/user/Login.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { useDispatch } from "react-redux";
-import { setUser, logout } from "./userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser, logout, selectUser } from "./userSlice";
 
 export default function Login() {
     const dispatch = useDispatch();
+    const user = useSelector(selectUser);
 
     const loginToApp = () => {
         dispatch(setUser({ user: "omaar_5" }));
-    }
-
+    };
+    
     return (
         <div>
             <h1 style={{ fontSize: 70 }}>I'm The Login Component</h1>
+            {user ? <h2>{user} Logged in</h2> : <h2>No Body Logged in</h2>}
             <button onClick={loginToApp}>Login</button>
             <button onClick={() => dispatch(logout())}>Logout</button>
         </div>
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,21 +2,25 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const userSlice = createSlice({
   name: 'user',
+
   initialState: {
-    value: null,
+    user: null,
   },
+
   reducers: {
     setUser: (state, action) => {
       state.user = action.payload.user;
     },
+
     logout: state => {
       state.user = null;
     },
+
   },
 });
 
 export const { setUser, logout } = userSlice.actions;
 
-export const selectUser = state => state.user;
+export const selectUser = state => state.user.user;
 
 export default userSlice.reducer;
